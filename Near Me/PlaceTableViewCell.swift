98e5a865e134bb769963ffd9d018716d6f0baa77
Kevin Mattappally <kevinmat@outlook.com>
2018-11-25 22:17:51 -0800

    table is showing

---
+++ Near Me/PlaceTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  PlaceTableViewCell.swift
+//  Near Me
+//
+//  Created by Kevin Mattappally on 11/25/18.
+//  Copyright © 2018 Kevin Mattappally. All rights reserved.
+//
+
+import UIKit
+
+class PlaceTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func update(with place: Place) {
+        
+    }
+
+}
--- Near Me/PlaceTableViewController.swift
+++ Near Me/PlaceTableViewController.swift
@@ -9,8 +9,27 @@
 import UIKit
 import CoreLocation
 
-class PlaceTableViewController: UIViewController {
+class PlaceTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    var places = [Place]()
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return places.count
+        }
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PlaceCell", for: indexPath) as! PlaceTableViewCell
+        let place = places[indexPath.row]
+        cell.update(with: place)
+        // cell.showsReorderControl = true
+        return cell
+    }
+    
+    
+    @IBOutlet var searchBar: UISearchBar!
     let apiKey: String = "AIzaSyBnyJwxj9eSbZKAUCW_6l1W3HYf8p5azXk"
 
     override func viewDidLoad() {
